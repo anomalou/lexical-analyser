210b50d0bc854f3c0a99e7ec3109dd36e95f29b2
Andrei Bikov <andrey07ey@gmail.com>
2022-03-16 00:27:25 +0300

    MVP

--- main.c
+++ main.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <string.h>
+#include <malloc.h>
 #define BUFFER_SIZE 256
 #define NUMBER_OF_STATES 23
 #define NUMBER_OF_SIGNAL 14
+#define LEX_ERROR "lex error"
 
 char buffer[BUFFER_SIZE];
 int buffer_offset = 0;
 
-char* lex_val;
+char lex_val[BUFFER_SIZE];
 int lex_length;
 
 typedef enum {
@@ -33,7 +35,7 @@ typedef enum {
     S19,
     S20,
     S21,
-    S_ERROR
+    S_ERROR = -100
 } state;
 
 int transitions[NUMBER_OF_STATES][NUMBER_OF_SIGNAL] = {
@@ -60,7 +62,7 @@ typedef enum {
     OTHER,
     ENDF,
     LETTER,
-    NUMBER,
+    NUMBER_SIGNAL,
     S_LETTER,
     W_LETTER,
     I_LETTER,
@@ -70,8 +72,17 @@ typedef enum {
     F_LETTER
 } input_signal;
 
+typedef enum {
+    IDENTIFICATOR,
+    NUMBER_LEXEM,
+    MATH_SIGN,
+    LOGIC_SIGN,
+    IF,
+    SWITCH
+} lexem;
+
 void appendToLex(char symbol) {
-    lex_val = strcat(lex_val, symbol);
+    lex_val[lex_length] = symbol;
     lex_length++;
 }
 
@@ -90,26 +101,47 @@ void reset_lex(){
     lex_length = 0;
 }
 
-char* state_machine() {
-    
+short isStateFinal(int state) {
+    return state == S3 || state == S10 || state == S12 || state == S16 || state == S18 || state == S21;
 }
 
-input_signal recognize(){
+char* state_machine() {
+    reset_lex();
+    
+    state cur_state = S0;
+    input_signal cur_input = recognize();
+
+    while (cur_input != ENDF) {
+        cur_state = transitions[cur_input][cur_state];
+        
+        if (cur_state == IDENTIFICATOR) 
+            return "IDENTIFICATOR";
+        if (cur_state == NUMBER_LEXEM) 
+            return "NUMBER_LEXEM";
+        if (cur_state == MATH_SIGN) 
+            return "MATH_SIGN";
+        if (cur_state == LOGIC_SIGN) 
+            return "LOGIC_SIGN";
+        if (cur_state == IF) 
+            return "IF";
+        if (cur_state == SWITCH) 
+            return "SWITCH";
+        
+        if (cur_state == S_ERROR || cur_input == ENDF)
+            return LEX_ERROR;
+
+    }
+}
+
+int recognize(){
 
     if (buffer_offset > BUFFER_SIZE - 1)
         return ENDF;
-    
 
     char letter = buffer[buffer_offset];
     buffer_offset++;
     
     input_signal result;
-
-    // if (isLetter() || isNumber) {
-    //     appendToLex(letter);
-    // } else {
-    //     reset_lex();
-    // }
 
     appendToLex(letter);
 
@@ -130,7 +162,7 @@ input_signal recognize(){
             if (isLetter(letter))
                 return LETTER;
             else if (isNumber(letter))
-                return NUMBER;
+                return NUMBER_SIGNAL;
             else
                 return OTHER;
     }
@@ -141,5 +173,15 @@ input_signal recognize(){
 int main() {
     printf("Hello world");
 
+    scanf("%s", buffer);
+    
+    char* status = malloc(sizeof(char) * BUFFER_SIZE);
+    
+    while (strcmp(status, LEX_ERROR) != 0)
+    {
+        strcpy(status, state_machine());
+        printf("%s: %s", lex_val, status);
+    }
+    
     return 0;
 }
